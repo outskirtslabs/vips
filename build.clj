38a770c736fcb51a238d591a579abbb0d6abc8f2
Casey Link <casey@outskirtslabs.com>
2026-09-09 17:33:19 +0200

    build: stop bundling the FFI runtime

--- build.clj
+++ build.clj
@@ -535,6 +535,7 @@
                             targets)})))
 
 (defn jar [_]
+  (b/delete {:path class-dir})
   (b/write-pom {:class-dir class-dir
                 :lib       lib
                 :version   version
@@ -549,12 +550,6 @@
                             (conj (url->scm repo-url-prefix) [:tag rev])]})
   (b/copy-dir {:src-dirs   (existing-paths ["src" "resources"])
                :target-dir class-dir})
-  ;; Maven cannot resolve Git coordinates. Bundle the pinned FFI runtime.
-  (let [ffi-dep (get-in @basis_ [:libs 'babashka/ffi])]
-    (b/copy-dir {:src-dirs   (:paths ffi-dep)
-                 :target-dir class-dir})
-    (copy-file! (str (io/file (:deps/root ffi-dep) "LICENSE"))
-                (str (io/file class-dir "META-INF/licenses/babashka-ffi/LICENSE"))))
   (b/jar {:class-dir class-dir
           :jar-file  jar-file}))
 
--- test/ol/vips_packaging_test.clj
+++ test/ol/vips_packaging_test.clj
@@ -1,71 +1,27 @@
 (ns ol.vips-packaging-test
   (:require
    [clojure.edn :as edn]
-   [clojure.java.io :as io]
    [clojure.java.shell :as shell]
-   [clojure.string :as str]
-   [clojure.test :refer [deftest is testing]]
-   [clojure.xml :as xml])
+   [clojure.test :refer [deftest is testing]])
   (:import
    [java.util.jar JarFile]))
 
-(defn- element-name
-  [element]
-  (some-> element :tag name keyword))
-
-(defn- child-text
-  [element child-name]
-  (some (fn [child]
-          (when (= child-name (element-name child))
-            (apply str (filter string? (:content child)))))
-        (:content element)))
-
-(defn- dependencies
-  [pom]
-  (->> (tree-seq map? :content pom)
-       (filter #(= :dependency (element-name %)))
-       (map (fn [dependency]
-              {:group-id    (child-text dependency :groupId)
-               :artifact-id (child-text dependency :artifactId)
-               :version     (child-text dependency :version)}))
-       set))
-
-(defn- jar-only-classpath
-  [jar-path]
-  (let [clojure-jars (->> (str/split (System/getProperty "java.class.path")
-                                     (re-pattern (java.util.regex.Pattern/quote java.io.File/pathSeparator)))
-                          (filter #(re-find #"[/\\](clojure|spec\.alpha|core\.specs\.alpha)-[^/\\]+\.jar$" %)))
-        native-paths (map #(str (io/file % "resources")) (.listFiles (io/file "native")))]
-    (str/join java.io.File/pathSeparator (concat [jar-path] clojure-jars native-paths))))
-
-(deftest main-jar-includes-ffi-runtime
+(deftest main-jar-does-not-bundle-dependency-runtime
   (let [project-version    (-> (slurp "deps.edn")
                                edn/read-string
                                (get-in [:aliases :neil :project :version]))
         jar-path           (format "target/vips-%s.jar" project-version)
         {:keys [exit err]} (shell/sh "clojure" "-T:build" "jar")]
     (is (zero? exit) err)
-    (with-open [jar (JarFile. jar-path)
-                pom (.getInputStream
-                     jar
-                     (.getJarEntry jar "META-INF/maven/com.outskirtslabs/vips/pom.xml"))]
-      (testing "Git dependency sources, resources, and license ship in the Maven jar"
-        (doseq [entry ["babashka/ffi.clj"
-                       "clj-kondo.exports/babashka/ffi/config.edn"
-                       "META-INF/licenses/babashka-ffi/LICENSE"]]
+    (with-open [jar (JarFile. jar-path)]
+      (testing "the jar contains the library and its Maven metadata"
+        (doseq [entry ["ol/vips.clj"
+                       "META-INF/maven/com.outskirtslabs/vips/pom.xml"
+                       "META-INF/maven/com.outskirtslabs/vips/pom.properties"]]
           (is (some? (.getJarEntry jar entry)) entry)))
-      (testing "the POM requires neither Coffi nor an unresolvable FFI Maven artifact"
-        (is (empty? (filter #(#{["org.suskalo" "coffi"] ["babashka" "ffi"]}
-                              [(:group-id %) (:artifact-id %)])
-                            (dependencies (xml/parse pom)))))))
-    (testing "the public API runs with only the built jar, Clojure, and native resources"
-      (let [expr                   '(do
-                                      (require '[ol.vips :as v] '[ol.vips.operations :as ops])
-                                      (with-open [image   (ops/black 3 2)
-                                                  decoded (v/from-buffer (v/write-to-buffer image ".png"))]
-                                        (prn (v/shape decoded))))
-            {:keys [exit out err]} (shell/sh "java" "--enable-native-access=ALL-UNNAMED"
-                                             "-cp" (jar-only-classpath jar-path)
-                                             "clojure.main" "-e" (pr-str expr))]
-        (is (zero? exit) err)
-        (is (= [3 2 1] (edn/read-string out)))))))
+      (testing "the jar cannot shadow a separately resolved FFI dependency"
+        (is (= []
+               (->> (enumeration-seq (.entries jar))
+                    (map #(.getName ^java.util.jar.JarEntry %))
+                    (filter #(re-find #"^(babashka/|coffi/|clj-kondo\.exports/babashka/ffi/|META-INF/licenses/babashka-ffi/)" %))
+                    vec)))))))
